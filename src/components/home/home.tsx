8231a9469a689d2d6a7c4259addbfdefb4bcaf63
chinmayshringi4@gmail.com <chinmayshringi4@gmail.com>
2021-05-06 21:54:04 +0530

    homebk

---
+++ src/components/home/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.css';
+import Navbar from '../nav/nav'
+function Home() {
+  return (
+    <div className="cont">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/nav/nav.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './nav.css';
+
+function Navbar() {
+  return (
+ <nav style={{}} className="navbar navbar-expand-lg navbar-light">
+  <a className="navbar-brand px-3" href="#">PizzaTemp</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item p-2">
+        <a className="nav-link" href="#">Home </a>
+      </li>
+      <li className="nav-item p-2">
+        <a className="nav-link" href="#">Order Now</a>
+      </li>
+      <li className="nav-item p-2">
+        <a className="nav-link" href="#">About Us</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  );
+}
+
+export default Navbar;
