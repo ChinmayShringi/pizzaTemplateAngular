d32f686e2a38031d066b8bdf3e27e876f9076d78
chinmayshringi4@gmail.com <chinmayshringi4@gmail.com>
2021-05-07 08:44:21 +0530

    home1

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,6 +5,16 @@ function Home() {
   return (
     <div className="cont">
       <Navbar/>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <h1>Pizza Temptation</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi adipisci, magni doloribus sequi recusandae assumenda et ullam ex inventore magnam, est sit mollitia placeat velit.</p>
+            
+  <button type="button" className="btn btn-warning">Order Today!</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -8,7 +8,7 @@ function Navbar() {
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+  {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item p-2">
         <a className="nav-link" href="#">Home </a>
@@ -20,7 +20,7 @@ function Navbar() {
         <a className="nav-link" href="#">About Us</a>
       </li>
     </ul>
-  </div>
+  </div> */}
 </nav>
   );
 }
